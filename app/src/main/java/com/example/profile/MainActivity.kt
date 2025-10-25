c31d7d9704563331227bc6c7d0b100ae423d240d
Rafie <rafieashraf716@gmail.com>
2025-10-25 16:45:40 +0700

    "Added login screen"

--- app/src/main/java/com/example/profile/MainActivity.kt
+++ app/src/main/java/com/example/profile/MainActivity.kt
@@ -13,7 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.profile.ui.theme.ProfileTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity  : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
